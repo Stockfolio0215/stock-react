7476fe3f2a1436d085f608972bb8f8ea343b4ca3
Stockfolio0215 <ourproject428@gmail.com>
2022-10-04 12:19:42 +0530

    secondcommit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Our Stock-folio -pp 
+          Stockfolio app for all the stocks. 
+          
         </p>
         <a
           className="App-link"
