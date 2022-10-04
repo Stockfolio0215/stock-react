d9cea69717a226b3ccdf0cbb18be83238fb8f476
Stockfolio0215 <ourproject428@gmail.com>
2022-10-04 12:12:39 +0530

    react-first-commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Our Stock folio app 
+          Our Stock-folio app 
         </p>
         <a
           className="App-link"
